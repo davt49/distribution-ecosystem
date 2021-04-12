d4262fc28ba644f44c9302e367d092612bbdc793
davidtsui <davidtsuiart@gmail.com>
2021-04-12 14:12:35 +0100

    frontpage

--- src/components/Start-page.js
+++ src/components/Start-page.js
@@ -13,8 +13,10 @@ class Startpage extends React.Component {
         <header>
           <div className='startpage-title'>Digital Distribution Ecosystem</div>
           <div className='startpage-subtitle'></div>
-          <div className='startpage-intro'>Written by David Tsui, August 2017 (Who is now a developer)</div>
-          <div className='startpage-author'>This is a small React.js project to turn an old essay of mine into a website!</div>
+          <div className='startpage-intro'>Written by David Tsui, August 2017 (Who is now a developer, 2021.)</div>
+          <div className='startpage-author'>
+            This is a small React.js project to turn an old university essay of mine into a website using the MERN stack. <br/>
+            More info on the code can be found on <a href="https://github.com/davt49/distribution-ecosystem" target="_blank">Github</a>.</div>
           <div className='startpage-start'>
            <a href='./Intro'>Start</a>
           </div>
